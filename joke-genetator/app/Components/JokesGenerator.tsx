5982f40146acb45d5f8cbf574aff8910346b999a
Megha Wadhwa <meghawadhwa20@gmail.com>
2024-10-14 13:10:56 +0530

    Jokes Generator added next joke button

--- joke-genetator/app/Components/JokesGenerator.tsx
+++ joke-genetator/app/Components/JokesGenerator.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import style from './JokesGenerator.module.css'
 
 interface Joke {
@@ -10,22 +11,32 @@ interface Joke {
 
 }
 
-const JokesGenerator = async () => {
-    const res = await fetch("https://api.chucknorris.io/jokes/random")
-    const jokes: Joke = await res.json()
-    console.log(jokes)
-    const { created_at, icon_url, id, url, value } = jokes;
-    const formattedDate = new Date(created_at).toISOString().split('T')[0];
+const JokesGenerator = () => {
+    const [joke, setJoke] = useState<Joke | null>(null)
+
+    const fetchJoke = async()=>{
+        const res = await fetch("https://api.chucknorris.io/jokes/random")
+        const jokes: Joke = await res.json()
+        setJoke(jokes)
+    }
+
+    useEffect(() => {
+        fetchJoke();
+      }, []);
+    
+      const formattedDate = joke ? new Date(joke.created_at).toISOString().split('T')[0] : '';
 
     return (
-        <div key={id} className={style.container}>
+        <div key={joke?.id} className={style.container}>
             <div className={style.card}>
             <h1 className={style.title}>Random Chuck Norris Jokes</h1>
-                <img src={icon_url} alt={url} />
+                <img src={joke?.icon_url} alt={joke?.url} />
             </div>
-                <p>{value}</p>
+                <p>{joke?.value}</p>
                 <p>Created at: {formattedDate}</p>
-                <button className={style.button}>Next Joke</button>
+                <button className={style.button} onClick={()=>{
+                     fetchJoke()
+                }}>Next Joke</button>
         </div>
     )
 }
